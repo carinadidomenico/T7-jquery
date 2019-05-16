d6600572f01834344884f6b855d44750d84bbc0c
carinadidomenico <carinadidomenico@gmail.com>
2019-05-16 09:56:55 -0300

    [AULA4]

---
+++ 04/tarot/public/js/script.js
@@ -0,0 +1,22 @@
+function carregaCarta () {
+    $.get("http://localhost:3000/cartas", trocaCarta);
+}
+
+function trocaCarta (cartas, status){
+    if (status == 'success') {
+        let numeroAleatorio = Math.floor (Math.random() * cartas.length);
+        $(".titulo").text(cartas[numeroAleatorio].nome);
+        $(".tipo").text(cartas[numeroAleatorio].tipo);
+        $(".descricao").text(cartas[numeroAleatorio].descricao);
+        $(".imagem").attr("src", cartas[numeroAleatorio].imagem);
+        $(".saiba-mais").attr("href", cartas[numeroAleatorio].link);
+
+    } else {
+        $(".mensagem").text ("Não foi possível carregar a carta.");
+    }
+}
+
+$(document).ready(function(){
+    carregaCarta();
+    $(".novaCarta").click (carregaCarta);
+});
